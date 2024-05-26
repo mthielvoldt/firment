4f5044a8fb2f4e6b75e99590c3918a28c8fed490
mthielvoldt <mthielvoldt@gmail.com>
2024-05-26 10:08:28 -0400

    get a start on gen-can.py

--- protocol/pb-plugins/gen-can.py
+++ protocol/pb-plugins/gen-can.py
@@ -1,15 +1,41 @@
 #!/usr/bin/env python3
 
 import sys
-from google.protobuf.compiler.plugin_pb2 import CodeGeneratorResponse
+# from file where carrier function is import CarrierFunction
+from google.protobuf.compiler.plugin_pb2 import CodeGeneratorResponse, CodeGeneratorRequest
+from google.protobuf.descriptor_pb2 import FileDescriptorProto, DescriptorProto
+from google.protobuf.descriptor import FieldDescriptor
 
-def generate_c() -> str:
-  return f"""
-/**
+header = """/**
 * Generated File, do not track!
 * Impelements functions to pack and unpack data structures for CAN tx/rx.
 */
   """
+
+def generate_c() -> str:
+  return header 
+
+def get_message_widget(message: DescriptorProto):
+  # spec a div with a name
+  message_name = message.name
+  field_strings = ""
+  # iterate through fields, adding an element for each type.
+  for field in message.field:
+    if field.type in numeric_fields:
+      field_class = message_name + '-' + str(field.number)
+      field_strings += f'''
+      code here for c files
+      '''
+  # enum_name = ""
+  # enum = next(enum for enum in proto.enum_type if enum.name == enum_name)
+  
+  return f'''
+int CANTransmit{message_name}({message_name}_t) {{
+  {field_strings}
+  return;
+}}
+
+'''
 
 if __name__ == "__main__":
   response = CodeGeneratorResponse()
