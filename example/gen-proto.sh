533ab99d191c3dcdbe3d8747bd398c796cfaf8c2
mthielvoldt <mthielvoldt@gmail.com>
2024-07-12 19:27:57 -0400

    move proto and gen-proto script to example

---
+++ example/gen-proto.sh
@@ -0,0 +1,4 @@
+protoc -I./ \
+  --plugin=protoc-gen-can=../protocol/ pb-plugins/gen-can.py --can_out=../shared-c \
+  --plugin=protoc-gen-widgets=../protocol/pb-plugins/gen-widgets.py --widgets_out=web-ui/src/generated \
+  mcu_1.proto 
--- protocol/gen-proto.sh
+++
@@ -1,4 +0,0 @@
-protoc -I./ \
-  --plugin=protoc-gen-can=pb-plugins/gen-can.py --can_out=../shared-c \
-  --plugin=protoc-gen-widgets=pb-plugins/gen-widgets.py --widgets_out=../example-web-ui/src/generated \
-  mcu_1.proto 
