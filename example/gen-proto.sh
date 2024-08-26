111a3436defe52f9fc974ed2ff4a8663aee3a03e
mthielvoldt <mthielvoldt@gmail.com>
2024-08-26 10:30:25 -0400

    nanopb generating code

--- example/gen-proto.sh
+++ example/gen-proto.sh
@@ -1,5 +1,7 @@
 source ../protocol/gen-venv/bin/activate
-protoc -I./ \
-  --plugin=protoc-gen-can=../protocol/pb-plugins/gen-can.py --can_out=firmware/generated \
+../protocol/nanopb/generator/protoc -I./ \
+  --nanopb_out=firmware/generated \
   --plugin=protoc-gen-widgets=../protocol/pb-plugins/gen-widgets.py --widgets_out=web-ui/src/generated \
   mcu_1.proto 
+
+#  --plugin=protoc-gen-can=../protocol/pb-plugins/gen-can.py --can_out=firmware/generated \
