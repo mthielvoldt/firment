a9298f85a4bdb40a0343c9caa77553be8f520049
mthielvoldt <mthielvoldt@gmail.com>
2024-08-27 19:07:47 -0400

    Try to support XMC4400 also

--- example/firmware/build-xmc.sh
+++ example/firmware/build-xmc.sh
@@ -1,3 +1,2 @@
 cmake --toolchain toolchain-xmc.cmake -B build/xmc -S.
 cmake --build build/xmc
-rm -r build/xmc
--- example/firmware/spi_example.c
+++ example/firmware/spi_example.c
@@ -1,6 +1,29 @@
 
+#ifdef XMC4400_F100x512
+#include "XMC4400.h"
+#endif
+
+#ifdef XMC4700_F144x2048
+#include "XMC4700.h"
+#endif
+
+#include "xmc_gpio.h"
+#include "xmc_common.h"
+
 int main(void) {
+  XMC_GPIO_PORT_t * const port = XMC_GPIO_PORT0;
+  const uint8_t pin = 13;
+  const XMC_GPIO_CONFIG_t pinConfig = {
+    .mode = XMC_GPIO_MODE_OUTPUT_PUSH_PULL,
+    .output_level = XMC_GPIO_OUTPUT_LEVEL_LOW,
+    .output_strength = XMC_GPIO_OUTPUT_STRENGTH_STRONG_SHARP_EDGE
+  };
+
+  XMC_GPIO_Init(port, pin, &pinConfig);
   for (;;) {
+
+    XMC_GPIO_ToggleOutput(port, pin);
+    for (volatile int i = 0; i < 400000; i++);
     
   }
 }
