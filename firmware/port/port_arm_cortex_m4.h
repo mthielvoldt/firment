4891a1780b63c0a33760491bf1d1d2619bf7f38d
mthielvoldt <mthielvoldt@gmail.com>
2024-09-16 10:29:17 -0400

    Add way to disable interrupts at-or-below a priority

---
+++ firmware/port/port_arm_cortex_m4.h
@@ -0,0 +1,23 @@
+#include <stdint-gcc.h>
+
+/**
+ * Stolen from vPortRaiseBASEPRI implementation found in:
+ * amazon-freertos/freertos_kernel/portable/GCC/ARM_CM4_MPU/portmacro.h
+ * 
+ * Nomenclature note: "High priority" ISRs preempt "Low priority" ISRs.
+ * When discussig numeric values of NVIC_prio_num, we use "lesser/greater" and
+ * "least/greatest".  So the "Highest priority" ISR will have the "least"
+ * NVIC_prio.
+ */
+inline static void disableLowPriorityInterrupts( uint32_t leastDisabledNVIC_prio )
+{
+	asm volatile
+	(
+		"	msr basepri, %0								\n" \
+		"	isb														\n" \
+		"	dsb														\n" \
+		:: "r" ( leastDisabledNVIC_prio ) : "memory"
+	);
+}
+
+#define enableAllInterrupts() disableLowPriorityInterrupts(0)
