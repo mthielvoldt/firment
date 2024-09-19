cae6190d864cbf82dcda9cd30f648bfd2e66f670
mthielvoldt <mthielvoldt@gmail.com>
2024-09-19 12:38:29 -0400

    comment example_comms.c

--- example/firmware/example_comms.c
+++ example/firmware/example_comms.c
@@ -1,3 +1,18 @@
+/** Comms module
+ * Concerns:
+ * - Executes system actions associated with valid inbound messages.
+ * - Constructs and queues outbound messages.
+ * - Does NOT do CRC validation - all inbound messages are assumed CRC-valid.
+ *
+ * Action types triggered by inbound messages:
+ * - change a parameter (triggers next action)
+ * - reporting value of requested parameter
+ * - general-purpose command (responses vary)
+ *
+ * Actions types assocated with outbound messages:
+ * - periodic telemetry (unrequested)
+ * - conditional-triggered alerts
+ */
 #include "example_comms.h"
 #include "fmt_spi.h"
 
@@ -12,27 +27,25 @@ void genTelem_periodic(void)
   switch (++count & (CALLS_PER_FULL_ROTATION - 1))
   {
   case 0:
+  {
     sendMsg((const Top){
         .which_sub = Top_WaveformTlm_tag,
         .sub = {
             .WaveformTlm = {
                 .currentMa = 5,
                 .voltageV = 4.3}}});
-
     break;
-  case 1:
+  }
+  case 20:
+  {
+    // sendMsg((const Top){
+    //     .which_sub = ,
+    //     .sub = {}});
     break;
-  case 2:
+  }
+  case 40:
     break;
-  case 3:
-    break;
-  case 4:
-    break;
-  case 5:
-    break;
-  case 6:
-    break;
-  case 7:
+  case 60:
     break;
   }
 }
