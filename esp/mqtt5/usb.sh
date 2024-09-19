f569596380d00a1ef4fed3111302e9c7ab9d4a1d
mthielvoldt <mthielvoldt@gmail.com>
2024-09-19 13:34:52 -0400

    make usb.sh detect the right device

--- esp/mqtt5/usb.sh
+++ esp/mqtt5/usb.sh
@@ -1,1 +1,5 @@
-source esptool-venv/bin/activate && esp_rfc2217_server.py -v -p 4000 /dev/ttyACM0
+source esptool-venv/bin/activate 
+# Find the correct device '/dev/ttyACM[0,1,2...]'
+
+ESP_NAME=$(ls /dev/serial/by-id | grep '^usb-1a86_USB')
+esp_rfc2217_server.py -v -p 4000 "/dev/serial/by-id/$ESP_NAME"
