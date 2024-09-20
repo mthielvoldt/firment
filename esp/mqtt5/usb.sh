dbce732e76c8b401a3c814b41deb4cb0979279f3
mthielvoldt <mthielvoldt@gmail.com>
2024-09-20 18:21:06 -0400

    Update USB.h to work with the direct-to-esp USB port

--- esp/mqtt5/usb.sh
+++ esp/mqtt5/usb.sh
@@ -2,4 +2,18 @@ source esptool-venv/bin/activate
 # Find the correct device '/dev/ttyACM[0,1,2...]'
 
 ESP_NAME=$(ls /dev/serial/by-id | grep '^usb-1a86_USB')
-esp_rfc2217_server.py -v -p 4000 "/dev/serial/by-id/$ESP_NAME"
+ESP_JTAG_NAME=$(ls /dev/serial/by-id | grep '^usb-Espressif_USB_JTAG_serial')
+
+echo $ESP_JTAG_NAME
+
+if [ -L "/dev/serial/by-id/$ESP_NAME" ]; then
+  echo "using ftdi usb..."
+  esp_rfc2217_server.py -v -p 4000 "/dev/serial/by-id/$ESP_NAME"
+
+elif [ -L "/dev/serial/by-id/$ESP_JTAG_NAME" ]; then
+  echo "using jtag usb..."
+  esp_rfc2217_server.py -v -p 4000 "/dev/serial/by-id/$ESP_JTAG_NAME"
+
+else
+  echo "neither file found."
+fi
