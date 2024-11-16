e34179e1785d96e8340b035a14d613b0e8cca988
mthielvoldt <mthielvoldt@gmail.com>
2024-11-16 16:18:38 -0500

    Make points per window editable

--- example/web-ui/src/Plot.tsx
+++ example/web-ui/src/Plot.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from "react";
+import { ChangeEvent, useEffect, useRef, useState } from "react";
 import { WebglPlot, WebglLine, ColorRGBA } from "webgl-plot";
 
 // Hand-adjustable parameters
@@ -11,11 +11,10 @@ const scaleY = 0.8;
 // Derived resources
 const pointsPerMsg = pointsPerSec / messagesPerSec;
 const secPerPoint = 1 / pointsPerSec;
-const numPoints = secPerWindow / secPerPoint; // aka points per window
 let prevDataTime = 0;
 let fpsCounter = 0;
-let wglp: (WebglPlot | null);
-let line: (WebglLine | null);
+let wglp: (WebglPlot | null) = null;
+let line: (WebglLine | null) = null;
 
 type prop = {
   freq: number;
@@ -24,18 +23,26 @@ type prop = {
 };
 
 export default function Plot({ freq, amp, noise }: prop) {
+  const [numPoints, setNumPoints] = useState(secPerWindow / secPerPoint);
   const canvas = useRef<HTMLCanvasElement>(null);
   const data = useRef([0]);
 
   // One-time setup: canvas size, handler for new data.
   useEffect(() => {
     if (canvas.current) {
-      const devicePixelRatio = window.devicePixelRatio || 1;
-      canvas.current.width = canvas.current.clientWidth * devicePixelRatio;
-      canvas.current.height = canvas.current.clientHeight * devicePixelRatio;
+      if (wglp === null) {
+        console.log("Setup canvas, wglp === null. numPoints: ", numPoints);
+        const devicePixelRatio = window.devicePixelRatio || 1;
+        canvas.current.width = canvas.current.clientWidth * devicePixelRatio;
+        canvas.current.height = canvas.current.clientHeight * devicePixelRatio;
+        wglp = new WebglPlot(canvas.current);
+      }
 
-      line = new WebglLine(new ColorRGBA(0.3, 1, 0.2, 1), numPoints);
-      wglp = new WebglPlot(canvas.current);
+      console.log("setup line with numPoints:", numPoints);
+      line = new WebglLine(
+        new ColorRGBA(Math.random() / 2 + 0.5, Math.random() / 2 + 0.5, Math.random(), 1),
+        numPoints);
+      wglp.removeAllLines();
       wglp.addLine(line);
       line.arrangeX();
 
@@ -57,19 +64,19 @@ export default function Plot({ freq, amp, noise }: prop) {
 
       // cleanup
       return () => {
+        console.log("Cleanup canvas.")
         clearInterval(intervalId);
-        wglp = null;
         line = null;
       };
     }
-
-  }, []);
+  }, [numPoints]);
 
   // Animation engine: updates frame when new data is available.
   useEffect(() => {
+    console.log("Setup animation.")
     let newFrame = () => {
       // Run this once every fpsDivider.
-      if ( wglp && (data.current.length > 0) && (fpsCounter >= fpsDivder) ) {
+      if (wglp && (data.current.length > 0) && (fpsCounter >= fpsDivder)) {
         fpsCounter = 0;
         wglp.linesData.forEach((line) => {
           const yArray = new Float32Array(data.current);
@@ -86,19 +93,30 @@ export default function Plot({ freq, amp, noise }: prop) {
     let id = requestAnimationFrame(newFrame);
 
     return () => {
+      console.log("Cleanup animation.")
       newFrame = () => { };
       cancelAnimationFrame(id);
     };
-  }, [freq, amp, noise]);
+  }, [freq, amp, noise, numPoints]);
 
   const canvasStyle = {
     width: "500px",
     height: "70vh"
   };
 
+  function changeNumPoints(e: ChangeEvent) {
+    e.preventDefault();
+    setNumPoints(Number.parseInt(e.currentTarget.value));
+  }
+
   return (
     <div>
       <canvas style={canvasStyle} ref={canvas} />
+      <label>
+        Num Points:
+        <input type="number" className="" value={numPoints} step={numPoints/2}
+          onChange={changeNumPoints} />
+      </label>
     </div>
   );
 }
