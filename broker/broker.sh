f421b862c48bd8804886db980aee211e8b79c919
mthielvoldt <mthielvoldt@gmail.com>
2024-11-22 12:04:12 -0500

    add my user id to mqtt docker run commands

--- broker/broker.sh
+++ broker/broker.sh
@@ -2,6 +2,7 @@ docker run \
   -it \
   --rm \
   --name broker \
+  -u $UID \
   --mount type=bind,source=./mosquitto.conf,target=/mosquitto/config/mosquitto.conf \
   -p 1883:1883 -p 8080:8080 \
   eclipse-mosquitto:2.0.18
--- broker/client.sh
+++ broker/client.sh
@@ -3,4 +3,5 @@ docker run \
   --rm \
   --name mq-client \
   --network host \
+  -u $UID \
   eclipse-mosquitto:2.0.18 mosquitto_sub --port 1883 -t "#"
