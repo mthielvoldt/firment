715da99eff7d0c4798d39ab8226b9fb917a3c025
mthielvoldt <mthielvoldt@gmail.com>
2024-12-06 15:42:24 -0500

    Certs permitting wss connection to localhost

--- broker/client.sh
+++ broker/client.sh
@@ -5,4 +5,4 @@ docker run \
   --network host \
   -u $UID \
   --mount type=bind,source=./certs,target=/mosquitto/certs \
-  eclipse-mosquitto:2.0.18 mosquitto_sub --port 1883 --cafile /mosquitto/certs/ca-root.crt -h 10.0.0.18 -t "#"
+  eclipse-mosquitto:2.0.18 mosquitto_sub --port 8883 --cafile /mosquitto/certs/broker.crt -h localhost -t "#" -d --insecure
