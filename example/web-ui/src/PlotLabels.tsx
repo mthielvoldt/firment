e81bd4dc171c25a683fd7703b068e16f5181859c
mthielvoldt <mthielvoldt@gmail.com>
2024-12-16 17:29:16 -0500

    Get x-axis labels using React

---
+++ example/web-ui/src/PlotLabels.tsx
@@ -0,0 +1,28 @@
+/** Labels are text elements overlaid on a canvas, which is managed elsewhere.
+ * there are 2 groups of labels: one for x-axis labels, one for y.  
+ * Ultimately, a label needs to know its position and text content. 
+ * Axis labels positionally and value-wise evenly spaced.  
+ */
+export interface AxisLabel {
+  position: { x: number, y: number };
+  text: string;
+};
+
+export function PlotLabels({ xAxis }: { xAxis: AxisLabel[] }) {
+  const labels = xAxis.map((label, i) => {
+    return (
+      <div key={i} className="plot-label" style={{
+        top: label.position.y,
+        left: label.position.x
+      }}>
+        {label.text}
+      </div>
+    );
+  });
+
+  return (
+    <div className="plot-labels-div">
+      {labels}
+    </div>
+  )
+}
