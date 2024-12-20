398af2038ba5144c1bf76bde37b6a48922230bb5
mthielvoldt <mthielvoldt@gmail.com>
2024-12-19 20:42:26 -0500

    Add connection-status indicator

--- example/web-ui/src/BrokerAddress.tsx
+++ example/web-ui/src/BrokerAddress.tsx
@@ -1,13 +1,35 @@
 import React, { useState } from "react";
 import { setupMq } from './mqclient';
 
+interface Status { colors: string; text: string };
+interface ConnectOptions {
+  connected: Status, stale: Status, connecting: Status, disconnected: Status
+};
+const statuses: ConnectOptions = {
+  connected: { colors: "success-colors", text: "Active Data" },
+  stale: { colors: "warn-colors", text: "Connected, No data" },
+  connecting: { colors: "warn-colors", text: "Connecting" },
+  disconnected: { colors: "problem-colors", text: "Not Connected" }
+};
 
 export default function BrokerAddress({ }) {
-  const [address, setAddress] = useState("localhost")
+  const [address, setAddress] = useState("localhost");
+  const [connectStatus, setConnectStatus] = useState<keyof ConnectOptions>("disconnected");
+  let staleTimeout: NodeJS.Timeout;
 
   function handleSubmit(e: React.FormEvent) {
     e.preventDefault();
-    setupMq(address);
+    clearTimeout(staleTimeout);
+    setConnectStatus("connecting");
+    setupMq(address, onSubscribe, onMessage);
+  }
+  function onSubscribe() {
+    setConnectStatus("stale");
+  }
+  function onMessage() {
+    clearTimeout(staleTimeout);
+    if (connectStatus !== "connected") setConnectStatus("connected");
+    staleTimeout = setTimeout( () => {setConnectStatus("stale")}, 2000);
   }
 
   return (
@@ -17,6 +39,9 @@ export default function BrokerAddress({ }) {
         <input type="text" value={address}
           onChange={(e) => setAddress(e.currentTarget.value)} />
       </label>
+      <span className={"pill " + statuses[connectStatus].colors}>
+        {statuses[connectStatus].text}
+      </span>
     </form>
   )
 }
