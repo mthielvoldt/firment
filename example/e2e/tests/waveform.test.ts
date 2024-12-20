fff75be294c8de729ecd2d514b7b006bbe0b6c0d
mthielvoldt <mthielvoldt@gmail.com>
2024-12-19 21:26:11 -0500

    Add broker connection tests

--- example/e2e/tests/waveform.test.ts
+++ example/e2e/tests/waveform.test.ts
@@ -2,27 +2,19 @@ import { test, expect, type Page } from '@playwright/test';
 
 test.beforeEach(async ({ page }) => {
   await page.goto('http://localhost:5173/firment/');
+  const ipInput = page.getByLabel("Broker IP Address:")
+  await ipInput.fill("listpalette.com");
+  ipInput.press('Enter');
+  await expect(page.getByText(/Active/)).toBeVisible();
 });
 
-test('Title is correct', async ({ page }) => {
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Embedded Project/);
-});
 
 test('WaveformCtl can enable and disable channel A', async ({ page }) => {
 
-  page.on("console", msg => console.log(msg.text()));
-
-  const currentMa = page.getByText("currentMa");
+  // page.on("console", msg => console.log(msg.text()));
   const voltageV = page.getByText("voltageV");
-
-  const ipInput = page.getByLabel("Broker IP Address:")
-  await ipInput.fill("listpalette.com");
-  await ipInput.press('Enter');
-  await expect(currentMa).toHaveText(/5/);
-
-
   const waveformCtl = page.getByRole("form", {name: "WaveformCtl"});
+
   await waveformCtl.getByLabel("channel").selectOption({index: 0});
   await waveformCtl.getByLabel("enabled").setChecked(false);
   await waveformCtl.getByLabel("shape").selectOption("SINE");
@@ -36,10 +28,4 @@ test('WaveformCtl can enable and disable channel A', async ({ page }) => {
   await waveformCtl.getByLabel("enabled").setChecked(true);
   await waveformCtl.getByRole("button", {name: "Send"}).click();
   await expect(voltageV).not.toHaveText(/0.000/);
-
-
-  // await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  // await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
