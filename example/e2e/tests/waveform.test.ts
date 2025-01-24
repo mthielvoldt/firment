afbe73691ad6245af6867f5a4b7c8e95bced5f11
mthielvoldt <mthielvoldt@gmail.com>
2025-01-24 11:11:27 -0500

    Make waveform.test more helpful when it fails
    - remove async actions from test.afterEach(), which fail after timeout.
    - add separate expect before the expect.toPass block to check if locators needed in the .toPass block find their elements.

--- example/e2e/tests/waveform.test.ts
+++ example/e2e/tests/waveform.test.ts
@@ -6,13 +6,10 @@ test.beforeEach(async ({ page }) => {
   await ipInput.fill("listpalette.com");
   ipInput.press('Enter');
   await expect(page.getByText(/Active/)).toBeVisible();
-});
-
-test.afterEach(async ({ page }) => {
   await page.getByRole("form", { name: "Reset" })
     .getByRole("button", { name: "Send" })
     .click();
-})
+});
 
 
 test('WaveformCtl can enable and disable channel A', async ({ page }) => {
@@ -37,14 +34,20 @@ test('WaveformCtl can enable and disable channel A', async ({ page }) => {
 });
 
 test('ChannelA shows right stats for Sin', async ({ page }) => {
-
-  // page.on("console", msg => console.log(msg.text()));
+  // let errorCount = 0;
+  // page.on("console", msg => {
+  //   if (msg.type() === "error") {
+  //     errorCount++;
+  //     // console.error(msg.text());
+  //   }
+  // });
 
   const waveformCtl = page.getByRole("form", { name: "WaveformCtl" });
 
   // Command sine wave at 1.2Hz, ranging 0 to 1 on Channel A
   await waveformCtl.getByLabel('enabled').check();
-  await waveformCtl.getByLabel("channel").selectOption({ index: 0 }); // Sine
+  await waveformCtl.getByLabel("channel").selectOption({ index: 0 }); // CHAN_A
+  await waveformCtl.getByLabel("shape").selectOption("DC")
   await waveformCtl.getByLabel('amplitudeV').fill('0.5');
   await waveformCtl.getByLabel('frequencyHz').fill('1');
   await waveformCtl.getByLabel('offsetV').fill('0.5');
@@ -53,17 +56,22 @@ test('ChannelA shows right stats for Sin', async ({ page }) => {
   // Turn on Scanning at 10Hz
   const scanCtl = page.getByLabel('RunScanCtl');
   await scanCtl.getByLabel('freq').selectOption('10');
-  await scanCtl.getByLabel('probe_0').selectOption('2');
+  await scanCtl.getByLabel('probe_0').selectOption('CHAN_A');
   await scanCtl.getByRole('button', { name: 'Send' }).click();
 
   // Switch plot view to active record.
   const recordInput = page.getByLabel('Record:');
   await recordInput.fill('1');
-  recordInput.press('Enter');
+  await recordInput.press('Enter');
+
+  const AAvgLocator = page.getByTestId('CHAN_A-ave');
+
+  // This expect makes it clear if the element is found.
+  await expect(AAvgLocator).toBeVisible({ timeout: 4000 });
 
   // Wait for average to be between 0.4 and 0.6 (it should converge on 0.5)
   await expect(async () => {
-    const avgAsString = await page.getByTestId('CHAN_A-ave').innerText();
+    const avgAsString = await AAvgLocator.innerText();
     const avg = Number(avgAsString);
     expect(avg).toBeLessThan(0.6);
     expect(avg).toBeGreaterThan(0.4);
