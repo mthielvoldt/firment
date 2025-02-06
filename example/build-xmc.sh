492b9179d0cc8b81d8bb81fe01769c929ed7c1c7
mthielvoldt <mthielvoldt@gmail.com>
2025-02-06 09:57:30 -0500

    Move venv creation to custom command.  Run config twice the first time in build-xmc.sh

--- example/build-xmc.sh
+++ example/build-xmc.sh
@@ -1,2 +1,11 @@
-cmake --toolchain toolchain-xmc.cmake -B build/xmc -S. && \
-  cmake --build build/xmc
+
+# If this is the first time configuring, run config for two passes
+# so subsequent config doesn't trigger re-building all c files.
+# Note: I'm not sure why two passes speeds up the next rebuild.
+if [ ! -d build/xmc ]; then
+  cmake --toolchain toolchain-xmc.cmake -B build/xmc -S.
+  echo "First pass configure done.  Toolchain set.
+  "
+fi
+
+cmake -B build/xmc -S. && cmake --build build/xmc
--- protocol/nanopb_py_env.cmake
+++ protocol/nanopb_py_env.cmake
@@ -12,22 +12,10 @@ else()
   set(VIRTUAL_ENV_ACTIVATE . ${VIRTUAL_ENV_DIR}/bin/activate)
 endif()
 
-# Create the virtual environment and pip install python packages. 
-if(NOT EXISTS "${VIRTUAL_ENV_DIR}")
-  message(STATUS "Creating virtual environment: ${VIRTUAL_ENV_DIR}")
-  execute_process(
-    COMMAND ${Python3_EXECUTABLE} -m venv "${VIRTUAL_ENV_DIR}" 
-    RESULT_VARIABLE result
-  )
-  if(result)
-      message(FATAL_ERROR "Failed to create virtual environment.")
-  endif()
-endif()
 
-
-# Create a custom target for installing dependencies. This is important so the dependencies are installed before any other target that needs them.
-add_custom_target(install_python_dependencies
-    COMMAND ${VIRTUAL_ENV_ACTIVATE} && python -m pip install ${PYTHON_PACKAGES} # Combined command
-    DEPENDS ${VIRTUAL_ENV_DIR} # Ensure the virtual env exists first
-    COMMENT "Installing Python dependencies"
+add_custom_command(
+  OUTPUT ${VIRTUAL_ENV_DIR}
+  COMMAND python3 -m venv "${VIRTUAL_ENV_DIR}" &&
+    ${VIRTUAL_ENV_ACTIVATE} &&
+    python -m pip install ${PYTHON_PACKAGES}
 )
