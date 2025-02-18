23386b32842cbd237ee3d468e783456b71eb5956
mthielvoldt <mthielvoldt@gmail.com>
2025-02-18 11:59:53 -0500

    Sending ImageData from ui. woot.

--- web-ui/src/Image.tsx
+++ web-ui/src/Image.tsx
@@ -1,26 +1,68 @@
 import { useState } from "react";
 import flashPageSize from "./generated/flashPage"
+import { sendMessage } from "./mqclient";
+
+function sendPage(data: ArrayBuffer, pageIndex: number, pageCount: number) {
+  // TODO: get max payload size from firment_msg_config.json: image-part-max-size
+  const imagePartMaxSize = 32;
+
+  const chunkCount = Math.ceil(data.byteLength / imagePartMaxSize);
+  let dataIdx = 0;
+  for (let chunkIndex = 0; chunkIndex < chunkCount; chunkIndex++ ) {
+    dataIdx = chunkIndex * imagePartMaxSize;
+    const payload = new Uint8Array(data.slice(dataIdx, dataIdx + imagePartMaxSize));
+    console.log("payload", payload);
+
+
+    sendMessage("ImageData", {
+      pageIndex,
+      pageCount,
+      chunkIndex,
+      chunkCountInPage: chunkCount,
+      payload,
+    })
+  }
+}
 
 export default function Image({}) {
-  // const [progressState, setProgressState] = useState("");
-  const [image, setImage] = useState("");
+  const [progress, setProgress] = useState("");
+  const [image, setImage] = useState(new ArrayBuffer());
 
-  function handleSubmit(e: React.FormEvent) {
-    e.preventDefault();
-    console.log(image);
+  async function  handleSubmit(submitEvent: React.FormEvent) {
+    submitEvent.preventDefault();
+    
+    const imageBytes = new Uint8Array(image);
+    console.log(imageBytes.slice(0, 512));
 
     // flashPageSize comes from a #define in a .h file in the firmware port,
     // through cmake 'flash_page_size' property on the MCUPort target.   
-    const numChunks = image.length / flashPageSize;
-    for (let chunkIndex = 0; chunkIndex < numChunks; chunkIndex++) {
+    const pageCount = Math.ceil(image.byteLength / flashPageSize);
+    setProgress(`Upload progres: 0/${pageCount}`);
 
+    for (let pageIdx = 0; pageIdx < 1; pageIdx++) {
+      const pageStartIdx = pageIdx * flashPageSize;
+      // slice won't extend past iterable's length, regardless of param: end. 
+      const pageData = image.slice(pageStartIdx, pageStartIdx + flashPageSize);
+      sendPage(pageData, pageIdx, pageCount);
+
+      // wait for page status.
+      // const status = await getPageStatus();
+      // if (status == WRITE_SUCCESS) {
+      //   setProgressState(`Upload progres: ${pageIdx}/${pageCount}`);
+      // }
+      // else if (status == WRITE_FAIL) {
+      //   setProgressState(`Upload Failed at page ${pageIdx}`);
+      // }
     }
+
+    setProgress("Upload complete.");
 
   }
   async function handleFileChange(e: React.ChangeEvent<HTMLInputElement> ) {
     e.preventDefault();
     if (e.currentTarget.files) {
-      const imageData = await e.currentTarget.files[0].text();
+      const imageData = await e.currentTarget.files[0].arrayBuffer();
+      // const imageU32 = new Uint32Array(imageData);
       setImage(imageData);
     }
   }
@@ -34,9 +76,10 @@ export default function Image({}) {
         <button type="submit">Send Image</button>
       </p>
       <label>Choose an image file
-        <input type="file" name="image-file" accept=".hex" 
+        <input type="file" name="image-file" accept=".bin" 
           onChange={handleFileChange}/>
       </label>
+      <p>{progress}</p>
 
     </form>
   )
