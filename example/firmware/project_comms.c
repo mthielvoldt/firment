f4a21645debed190124c303ee0e0ac60844023ad
mthielvoldt <mthielvoldt@gmail.com>
2025-02-18 23:52:58 -0500

    handleImageData erasing and writing flash

--- example/firmware/project_comms.c
+++ example/firmware/project_comms.c
@@ -16,11 +16,13 @@
 #include "project_comms.h"
 #include "control.h"
 #include <fmt_log.h>
+#include <fw_update.h>
+#include <fmt_flash.h>
 
 #define CALLS_PER_FULL_ROTATION 1000U
 
 // optimization: stop storing the config.
-static portPin_t led = {0};
+static portPin_t led;
 
 void comm_init(portPin_t ledPin)
 {
@@ -70,24 +72,80 @@ void handleWaveformCtl(WaveformCtl msg)
   fmt_sendLog(LOG_INFO, "WaveformCtl rx cnt: ", count);
 
   const waveShape_t toControlShape[] = {
-    [WaveShape_UNSPECIFIED] = WAVE_SHAPE_DC,
-    [WaveShape_SINE] = WAVE_SHAPE_SINE,
-    [WaveShape_SQUARE] = WAVE_SHAPE_SQUARE,
-    [WaveShape_SAWTOOTH] = WAVE_SHAPE_SAWTOOTH,
-    [WaveShape_DC] = WAVE_SHAPE_DC,
+      [WaveShape_UNSPECIFIED] = WAVE_SHAPE_DC,
+      [WaveShape_SINE] = WAVE_SHAPE_SINE,
+      [WaveShape_SQUARE] = WAVE_SHAPE_SQUARE,
+      [WaveShape_SAWTOOTH] = WAVE_SHAPE_SAWTOOTH,
+      [WaveShape_DC] = WAVE_SHAPE_DC,
   };
   // opportunity here to limit input and notify if command exceeds limits.
   const waveCfg_t cfg = {
-    .id = msg.channel,
-    .amplitudeV = msg.enabled? msg.amplitudeV : 0.0F,
-    .frequencyHz = msg.frequencyHz, 
-    .offsetV = msg.enabled ? msg.offsetV : 0.0F,
-    .shape = toControlShape[msg.shape]
-  };
+      .id = msg.channel,
+      .amplitudeV = msg.enabled ? msg.amplitudeV : 0.0F,
+      .frequencyHz = msg.frequencyHz,
+      .offsetV = msg.enabled ? msg.offsetV : 0.0F,
+      .shape = toControlShape[msg.shape]};
   ctl_setWaveform(cfg);
 }
 
 void handleReset(Reset msg)
 {
   NVIC_SystemReset();
+}
+
+static void sendPageStatus(uint32_t pageIndex, PageStatusEnum status)
+{
+  fmt_sendMsg((const Top){
+      .which_sub = Top_PageStatus_tag,
+      .sub = {
+          .PageStatus = {
+              .pageIndex = pageIndex,
+              .status = status}}});
+}
+
+void handleImageData(ImageData msg)
+{
+  static uint32_t chunkRxCount = 0;
+  static uint32_t activePage = 0;
+  static uint8_t pageBuffer[FLASH_PAGE_SIZE];
+  static uint8_t *writeAdrs = pageBuffer;
+
+  bool bufferOverflow =
+    (writeAdrs + msg.payload.size) > (pageBuffer + FLASH_PAGE_SIZE);
+  bool wrongPage = msg.pageIndex != activePage;
+  bool badPageCount = 
+    msg.pageCount > (IMAGE_RX_SECTOR_SIZE / FLASH_PAGE_SIZE) || !msg.pageCount;
+
+  // Error: check for pageIndex getting out-of-sync with active page.
+  if (bufferOverflow || wrongPage || badPageCount)
+  {
+    sendPageStatus(activePage, PageStatusEnum_WRITE_FAIL);
+    activePage = 0;
+    writeAdrs = pageBuffer;
+    chunkRxCount = 0;
+    return;
+  }
+
+  memcpy(writeAdrs, msg.payload.bytes, msg.payload.size);
+  writeAdrs += msg.payload.size;
+
+  if (++chunkRxCount == msg.chunkCountInPage)
+  {
+    // If we're ready to write the first page, erase the sector. 
+    if (activePage == 0)
+    {
+      hal_flash_erase(IMAGE_RX_ADDRESS, IMAGE_RX_SECTOR_SIZE);
+    }
+    hal_flash_write(
+        IMAGE_RX_ADDRESS + activePage * FLASH_PAGE_SIZE,
+        pageBuffer,
+        FLASH_PAGE_SIZE);
+    writeAdrs = pageBuffer;
+    chunkRxCount = 0;
+    if (++activePage == msg.pageCount)
+    {
+      activePage = 0;
+    }
+    sendPageStatus(activePage, PageStatusEnum_WRITE_SUCCESS);
+  }
 }
