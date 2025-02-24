22cf6806a5312e41cdfa8af460eec915e231d459
mthielvoldt <mthielvoldt@gmail.com>
2025-02-23 21:55:17 -0500

    Increase Rx queue size in fmt_spi to accommodate FW updates

--- firmware/fmt_sizes.h
+++ firmware/fmt_sizes.h
@@ -9,6 +9,7 @@
 4: count, 4 value, 3 for the 3 log types, 2 for top types, 1 prefix.*/
 #define MAX_LOG_TEXT_SIZE (MAX_PACKET_SIZE_BYTES - 17)
 #define SEND_QUEUE_LENGTH 4U
+#define RX_QUEUE_LENGTH 9U
 #define MAX_SENDER_PRIORITY 16U
 
 #define MAX_MESSAGE_SIZE_BYTES (MAX_PACKET_SIZE_BYTES - PREFIX_SIZE_BYTES)
