1be2997a8cf0d42689651cea4898b2488c2fcca7
mthielvoldt <mthielvoldt@gmail.com>
2025-03-06 14:06:12 -0500

    Copy project_comms.h to test.  (todo 12,13,14)

--- test/firmware/project_comms.h
+++ test/firmware/project_comms.h
@@ -1,10 +1,14 @@
 /** Gives Firment access to the pb message data types for this project.
  * Also defines which of those messages are in-use in this project.
+ * 
+ * TODO: (12) Can I stop including this .h in fmt_rx and remove dup from test?
+ * TODO: (13) Should USE_xxx be in firmentConfig - way to turn on/off modules?
  */
 #pragma once
 #include "gpio_common.h"
 #include <fmt_comms.h>
 
+// TODO: (14) feature envy: gpio really doesn't belong in this header.
 void comm_init(portPin_t ledPin); // kinda hate xmc gpio being here - for main, not fmt.
 
 #define USE_WaveformCtl
