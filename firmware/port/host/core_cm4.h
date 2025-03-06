5a222d5fb62337753329a4328d1aa4501bd7630a
mthielvoldt <mthielvoldt@gmail.com>
2025-03-06 11:28:41 -0500

    Add core_cm4.h defining NVIC priority utils. (todo4)

---
+++ firmware/port/host/core_cm4.h
@@ -0,0 +1,46 @@
+#pragma once
+
+// TODO: (4) Make these spies.  Understand requirements for proper use.
+#include <stdint.h>
+
+// Hardware specific.  6 consistent with XMC4700.h
+#define __NVIC_PRIO_BITS 6 /*!< Number of Bits used for Priority Levels                               */
+
+/**
+ * An exact copy of CMSIS 6.1.0 implementation.
+ */
+static inline uint32_t NVIC_EncodePriority (uint32_t PriorityGroup, uint32_t PreemptPriority, uint32_t SubPriority)
+{
+  uint32_t PriorityGroupTmp = (PriorityGroup & (uint32_t)0x07UL);   /* only values 0..7 are used          */
+  uint32_t PreemptPriorityBits;
+  uint32_t SubPriorityBits;
+
+  PreemptPriorityBits = ((7UL - PriorityGroupTmp) > (uint32_t)(__NVIC_PRIO_BITS)) ? (uint32_t)(__NVIC_PRIO_BITS) : (uint32_t)(7UL - PriorityGroupTmp);
+  SubPriorityBits     = ((PriorityGroupTmp + (uint32_t)(__NVIC_PRIO_BITS)) < (uint32_t)7UL) ? (uint32_t)0UL : (uint32_t)((PriorityGroupTmp - 7UL) + (uint32_t)(__NVIC_PRIO_BITS));
+
+  return (
+           ((PreemptPriority & (uint32_t)((1UL << (PreemptPriorityBits)) - 1UL)) << SubPriorityBits) |
+           ((SubPriority     & (uint32_t)((1UL << (SubPriorityBits    )) - 1UL)))
+         );
+}
+
+/**
+  \brief   Get Priority Grouping
+  \details Reads the priority grouping field from the NVIC Interrupt Controller.
+  \return                Priority grouping field (SCB->AIRCR [10:8] PRIGROUP field).
+ */
+uint32_t NVIC_GetPriorityGrouping(void)
+{
+  return 0;
+}
+
+/**
+  \brief   Set Interrupt Priority
+  \details Sets the priority of a device specific interrupt or a processor exception.
+           The interrupt number can be positive to specify a device specific interrupt,
+           or negative to specify a processor exception.
+  \param [in]      IRQn  Interrupt number.
+  \param [in]  priority  Priority to set.
+  \note    The priority cannot be set for every processor exception.
+ */
+void NVIC_SetPriority(int32_t IRQn, uint32_t priority) {}
