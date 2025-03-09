100acca3e091773bace79ab1d62f747f284bd044
mthielvoldt <mthielvoldt@gmail.com>
2025-03-09 11:34:09 -0400

    Placeholder for gpioTest

---
+++ firmware/test/gpioTest.cpp
@@ -0,0 +1,22 @@
+#include <CppUTest/TestHarness.h>
+
+extern "C" {
+  #include <fmt_gpio.h>
+}
+
+TEST_GROUP(fmt_gpio)
+{
+  void setup()
+  {
+
+  }
+  void teardown()
+  {
+
+  }
+};
+
+TEST(fmt_gpio, initInputPin)
+{
+  FAIL("Here1");
+}
