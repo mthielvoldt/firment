dd22f4225b95360ea12d3cc3010768ff082819a1
mthielvoldt <mthielvoldt@gmail.com>
2025-03-12 09:20:28 -0400

    Comments refining

--- example/firmware/config/gpio_pcb.h
+++ example/firmware/config/gpio_pcb.h
@@ -1,6 +1,14 @@
 
 #include <mcu_gpio.h>
 
+/**
+ * To prepare a new GPIO for use: 
+ * 1. associate the new pin to its purpose with a descriptive name.
+ * 2. add this new name to AVAILABLE_GPIOs
+ * 3. #define the PIN_ID corresponding to the position of the new pin in 
+ *    AVAILABLE_GPIOs.
+ */
+
 #define LED_0_PIN P5_9
 #define LED_1_PIN P5_8
 
