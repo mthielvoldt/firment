8f1387da605eb4a82e08f8c2675f8ba4af39f238
mthielvoldt <mthielvoldt@gmail.com>
2025-04-08 00:00:45 -0400

    All targets building

--- example/config/toolchain-xmc.cmake
+++ example/config/toolchain-xmc.cmake
@@ -2,11 +2,10 @@ set(CMAKE_SYSTEM_NAME Generic)
 set(CMAKE_SYSTEM_PROCESSOR arm)
 
 set(CMAKE_C_STANDARD 11)
-set(CMAKE_C_EXTENSIONS OFF)
+# set(CMAKE_C_EXTENSIONS OFF)
 
 # Suppresses the check for a working compiler, which will fail for sys calls since there's no sys.
 set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
 
 set(CMAKE_C_COMPILER arm-none-eabi-gcc)
 set(CMAKE_C_OBJCOPY arm-none-eabi-objcopy)
-set(CMAKE_C_FLAGS_DEBUG "-Og -g")
