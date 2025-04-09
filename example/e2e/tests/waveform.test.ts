c144c9c6d472bc54d44873332c3ead8c4b04ca26
mthielvoldt <mthielvoldt@gmail.com>
2025-04-09 17:36:35 -0400

    E2E in CI try 1

--- example/e2e/tests/waveform.test.ts
+++ example/e2e/tests/waveform.test.ts
@@ -1,7 +1,7 @@
 import { test, expect, type Page } from '@playwright/test';
 
 test.beforeEach(async ({ page }) => {
-  await page.goto('http://localhost:5173/firment/');
+  await page.goto('http://localhost:4173/firment/');
   const ipInput = page.getByLabel("Broker IP Address:")
   await ipInput.fill("listpalette.com");
   ipInput.press('Enter');
