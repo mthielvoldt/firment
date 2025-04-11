becabba36eb6b11ffa50065863fae015674c6f53
mthielvoldt <mthielvoldt@gmail.com>
2025-04-11 14:33:23 -0400

    Add setBuildIdGetter extension to fmt_version

--- firmware/fmt_version.c
+++ firmware/fmt_version.c
@@ -1,15 +1,27 @@
 #include <fmt_version.h>
 #include "fmt_comms.h"
 
+static uint32_t (*buildIdGetter)(void) = NULL;
+
+void fmt_setBuildIdGetter(uint32_t (*getter)(void))
+{
+  buildIdGetter = getter;
+}
+
 bool fmt_sendVersion(void)
 {
   Top msg = {
-    .which_sub = Top_Version_tag,
-    .sub = {.Version = {
-      .major = VERSION_MAJOR,
-      .minor = VERSION_MINOR,
-      .patch = VERSION_PATCH,
-    }}
+      .which_sub = Top_Version_tag,
+      .sub = {
+          .Version = {
+              .major = VERSION_MAJOR,
+              .minor = VERSION_MINOR,
+              .patch = VERSION_PATCH,
+          },
+      },
   };
+  if (buildIdGetter)
+    msg.sub.Version.buildId = buildIdGetter();
+
   return fmt_sendMsg(msg);
 }
--- firmware/test/versionTest.cpp
+++ firmware/test/versionTest.cpp
@@ -18,12 +18,21 @@ extern "C"
 #include "stub_comms.h"
 }
 
+#define TEST_BUILD_ID 1234U
+
+static uint32_t getBuildID(void) 
+{
+  return TEST_BUILD_ID;
+}
 
 TEST_GROUP(fmt_version)
 {
+  Top msg;
   void setup()
   {
     UT_PTR_SET(fmt_sendMsg, fmt_sendMsg_test);
+    msg = (const Top){0};
+    fmt_setBuildIdGetter(NULL);
   }
   void teardown()
   {
@@ -32,11 +41,26 @@ TEST_GROUP(fmt_version)
 
 TEST(fmt_version, sendVersion_succeeds)
 {
-  Top msg;
   CHECK(fmt_sendVersion());
   CHECK(fmt_getMsg_test(&msg));
   CHECK_EQUAL(Top_Version_tag, msg.which_sub);
   CHECK_EQUAL(VERSION_MAJOR, msg.sub.Version.major);
   CHECK_EQUAL(VERSION_MINOR, msg.sub.Version.minor);
   CHECK_EQUAL(VERSION_PATCH, msg.sub.Version.patch);
+  CHECK_EQUAL_ZERO(msg.sub.Version.buildId);
+}
+
+TEST(fmt_version, setBuildIdGetter_handlesNulls)
+{
+  CHECK(fmt_sendVersion());
+  CHECK(fmt_getMsg_test(&msg));
+  CHECK_EQUAL_ZERO(msg.sub.Version.buildId);
+}
+
+TEST(fmt_version, buildTime_isAccurate)
+{
+  fmt_setBuildIdGetter(getBuildID);
+  CHECK(fmt_sendVersion());
+  CHECK(fmt_getMsg_test(&msg));
+  CHECK_EQUAL(TEST_BUILD_ID, msg.sub.Version.buildId);
 }
