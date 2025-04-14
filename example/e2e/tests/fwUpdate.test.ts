322b7d4c1d3bc68340c9bf45483b9fd81aa1f668
mthielvoldt <mthielvoldt@gmail.com>
2025-04-14 17:05:01 -0400

    Build Id visible e2e test

--- example/e2e/tests/fwUpdate.test.ts
+++ example/e2e/tests/fwUpdate.test.ts
@@ -1,5 +1,15 @@
 import { test, expect, type Page } from '@playwright/test';
-import path = require('node:path');
+import * as path from 'path';
+import * as fs from 'fs';
+
+function getBuildIdData() {
+  const appBinary = path.resolve(
+    __dirname, "../../build/xmc/mgt-boot/app_offset_signed.bin")
+  const buildTimeFile = path.resolve(
+    __dirname, '../../build/xmc/build_time.txt');
+  const appBuildId = fs.readFileSync(buildTimeFile, 'ascii');
+  return {appBinary, appBuildId}
+}
 
 test.beforeEach(async ({ page }) => {
   await page.goto('http://localhost:4173/firment/');
@@ -12,13 +22,20 @@ test.beforeEach(async ({ page }) => {
     .click();
 });
 
+test('Build Id is visible', async ({ page }) => {
+  const buildId = page.getByTestId("fw-build-id");
 
-test('Update the firmware', async ({ page }) => {
+  const {appBuildId} = getBuildIdData()
+
+  await expect(buildId).toHaveText(appBuildId);
+})
+
+test.skip('Update the firmware', async ({ page }) => {
 
   // page.on("console", msg => console.log(msg.text()));
   const imageFileInput = page.getByLabel("Choose an image file");
   await imageFileInput.setInputFiles(
-    path.join(__dirname, '..', '..', 'build', 'xmc', 'mgt-boot', 'app_offset_latest_signed.bin'));
+    path.join(__dirname, '..', '..', 'build', 'xmc', 'mgt-boot', 'app_offset_signed.bin'));
   
   await page.getByRole("button", {name: "Send Image"}).click();
 
