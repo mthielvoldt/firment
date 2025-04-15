97d695db17ff8c034f693793df9ca6e451c3437b
mthielvoldt <mthielvoldt@gmail.com>
2025-04-14 20:25:53 -0400

    FW update test passing

--- example/e2e/tests/fwUpdate.test.ts
+++ example/e2e/tests/fwUpdate.test.ts
@@ -3,8 +3,8 @@ import * as path from 'path';
 import * as fs from 'fs';
 
 function getBuildIdData() {
-  const appBinary = path.resolve(
-    __dirname, "../../build/xmc/mgt-boot/app_offset_signed.bin")
+  const appBinary = path.resolve( __dirname, 
+    "../../build/xmc/mgt-boot/app_offset_signed.bin")
   const buildTimeFile = path.resolve(
     __dirname, '../../build/xmc/build_time.txt');
   const appBuildId = fs.readFileSync(buildTimeFile, 'ascii');
@@ -25,28 +25,43 @@ test.beforeEach(async ({ page }) => {
 test('Build Id is visible', async ({ page }) => {
   const buildId = page.getByTestId("fw-build-id");
 
-  const {appBuildId} = getBuildIdData()
+  await expect(async () => {
+    const id = Number(await buildId.innerText());
+    expect(id).toBeLessThan(Date.now()/1000);
+    expect(id).toBeGreaterThan(1744665208);
+  }).toPass({ intervals: [250], timeout: 2000 });
+});
 
-  await expect(buildId).toHaveText(appBuildId);
-})
 
-test.skip('Update the firmware', async ({ page }) => {
+test('Update the firmware', async ({ page }) => {
+  const longTimeout = 3*60_000;
+  test.setTimeout(longTimeout + 60_000);
+  const fastExpect = expect.configure({timeout: 1000});
+  const slowExpect = expect.configure({timeout: longTimeout});
 
   // page.on("console", msg => console.log(msg.text()));
   const imageFileInput = page.getByLabel("Choose an image file");
-  await imageFileInput.setInputFiles(
-    path.join(__dirname, '..', '..', 'build', 'xmc', 'mgt-boot', 'app_offset_signed.bin'));
+  const buildId = page.getByTestId("fw-build-id");
+  const {appBinary, appBuildId} = getBuildIdData();
+  const progress = page.getByTestId("upload-progress");
+  const resetBtn = page.getByRole("form", { name: "Reset" })
+    .getByRole("button", { name: "Send" })
+
+  await expect(async () => {
+    const id = Number(await buildId.innerText());
+    expect(id).toBeLessThan(Number(appBuildId));
+    expect(id).toBeGreaterThan(1744665208);
+  }).toPass({ intervals: [250], timeout: 2000 });
+
+  await imageFileInput.setInputFiles(appBinary);
+  await fastExpect(imageFileInput).toHaveValue(/app_offset_signed.bin/);
   
   await page.getByRole("button", {name: "Send Image"}).click();
 
-  const buildId = page.getByTestId("fw-build-id");
+  await fastExpect(progress).toHaveText(/in progress/);
+  await slowExpect(progress).toHaveText(/complete/);
+  await resetBtn.click();
 
-  // Improve how to unambiguously identify the FW version. 
-  // Fix whatevers making the version not show up. 
-
-  // Get the version we just allegedly uploaded from "version.txt". 
-  // look for a match with whats on screen (after a wait)
-  // End early if there's a failure notice.
-  await expect(buildId).toHaveText("0.2.0");
-  
+  // TODO: End early if there's a failure notice.
+  await slowExpect(buildId).toHaveText(appBuildId);
 });
