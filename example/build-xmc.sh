8265d4b16a4c09be75e7919d1f59412a67598707
mthielvoldt <mthielvoldt@gmail.com>
2025-04-29 13:48:58 -0400

    refactor: rename toolchain file

--- example/build-xmc.sh
+++ example/build-xmc.sh
@@ -3,7 +3,7 @@
 # so subsequent config doesn't trigger re-building all c files.
 # Note: I'm not sure why two passes speeds up the next rebuild.
 if [ ! -d build/xmc ]; then
-  cmake --toolchain config/toolchain-xmc.cmake -B build/xmc -S.
+  cmake --toolchain config/toolchain-arm-gcc.cmake -B build/xmc -S.
   echo "First pass configure done.  Toolchain set.
   "
 fi
