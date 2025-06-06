799251e4ddd069a39914df0c71cd539b17df9ba7
mthielvoldt <mthielvoldt@gmail.com>
2025-06-06 00:42:45 -0400

    App builds

--- example/firmware/init_spi_pcb1.c
+++ example/firmware/init_spi_pcb1.c
@@ -10,11 +10,12 @@
 #include <ioc_mcuDetails.h> // IOC_x_to_ISR_y mcu-supported routing options
 #include <priority.h>
 
-extern ARM_DRIVER_SPI Driver_SPI4;
+SPI_HandleTypeDef hspi3;
+extern ARM_DRIVER_SPI Driver_SPI3;
 
 spiCfg_t spiConfig = {
   .spiModuleId = 4,
-  .spiModule = &Driver_SPI4,
+  .spiModule = &Driver_SPI3,
   .msgWaitingIocId = IOC_14_RTE_INDEX, // IOC_14: P1_0 on XMC4700
   .msgWaitingIocOut = 0,  // not used in stm32; no output routing stage in IOC
   .clearToSendIocId = IOC_4_RTE_INDEX, // IOC_4:  P0_4 on XMC4700
