68c491d012466027c0f0a9ba4a42b4e4f46aa382
mthielvoldt <mthielvoldt@gmail.com>
2025-06-22 19:26:57 -0400

    feat: partition geometry is MCU specific

--- example/config/partitions.cmake
+++ example/config/partitions.cmake
@@ -6,14 +6,25 @@
 # "DIRECT" implies non-cached access, which must be used for program/erase.
 # In MCUs without Pmem cache, "DIRECT" and "CACHED" values will be the same.
 
-set(PARTITION_SIZE              0x40000) # 512kB
-set(SECTOR_SIZE                 0x40000) # 256kB (5.5s erases)
-set(PMEM_ROOT_ADDRESS_DIRECT        0x0C000000) # ARCH_FLASH_OFFSET_DIRECT
-set(PMEM_ROOT_ADDRESS_CACHED        0x08000000)
-set(PARTITION_ACTIVE_ADDRESS_DIRECT 0x0C040000) # sector 9
-set(PARTITION_ACTIVE_ADDRESS_CACHED 0x08040000) # for linker
-set(PARTITION_UPDATE_ADDRESS        0x0C080000) # sector 10
-set(PARTITION_BACKUP_ADDRESS        0x0C0C0000) # sector 11
+if(${MCU_VARIANT} STREQUAL "XMC4700")
+  set(PARTITION_SIZE              0x40000) # 256kB
+  set(SECTOR_SIZE                 0x40000) # 256kB (5.5s erases)
+  set(PMEM_ROOT_ADDRESS_DIRECT        0x0C000000) # ARCH_FLASH_OFFSET_DIRECT
+  set(PMEM_ROOT_ADDRESS_CACHED        0x08000000)
+  set(PARTITION_ACTIVE_ADDRESS_DIRECT 0x0C040000) # sector 9
+  set(PARTITION_ACTIVE_ADDRESS_CACHED 0x08040000) # for linker
+  set(PARTITION_UPDATE_ADDRESS        0x0C080000) # sector 10
+  set(PARTITION_BACKUP_ADDRESS        0x0C0C0000) # sector 11
+elseif(${MCU_VARIANT} STREQUAL "stm32l476")
+  set(PARTITION_SIZE              0x40000) # 256kB
+  set(SECTOR_SIZE                 0x800) # 2kB (sector = erase block)
+  set(PMEM_ROOT_ADDRESS_DIRECT        0x08000000) # bank 1
+  set(PMEM_ROOT_ADDRESS_CACHED        0x08000000) # no cache; same as prev.
+  set(PARTITION_ACTIVE_ADDRESS_DIRECT 0x08040000) # bank 1
+  set(PARTITION_ACTIVE_ADDRESS_CACHED 0x08040000) # no cache; same as prev.
+  set(PARTITION_UPDATE_ADDRESS        0x08080000) # bank 2
+  set(PARTITION_BACKUP_ADDRESS        0x080C0000) # bank 2
+endif()
 
 # Arm MCUs often have alignment requirements for the vector table.
 # Refer to for Vector Table Offset Register (VTOR) section in MCU reference.
