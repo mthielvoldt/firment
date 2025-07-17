3ee995df8d9d38d9c9bde024b14ce10fd9b9258e
mthielvoldt <mthielvoldt@gmail.com>
2025-07-17 19:46:08 -0400

    feat: add initClock() to speed up bootloader refactor: rename mgt-boot adapter code

--- example/config/partitions.cmake
+++ example/config/partitions.cmake
@@ -3,8 +3,24 @@
 #
 # These values will be used to configure partitions.h, used by the bootloader.
 #
-# "DIRECT" implies non-cached access, which must be used for program/erase.
+# Details
+# ===========================================================================
+# In partitions, "DIRECT" implies non-cached access, used for program/erase.
 # In MCUs without Pmem cache, "DIRECT" and "CACHED" values will be the same.
+#
+# TEST_RESULT_ADDRESS
+# To ensure new images are capable of downloading new FW images, the BL expects
+# the application to download and write a nonce to flash at TEST_RESULT_ADDRESS
+# the first time it it runs.  
+# On subsequent boots, the BL will check for this nonce to be present,
+# and will restore the backup image if the nonce is not present or invalid.
+#
+# IMAGE_HEADER_SIZE
+# Arm MCUs often have alignment requirements for the vector table.  Refer to 
+# Vector Table Offset Register (VTOR) section in MCU reference.
+# Generally, the alignment required is the power of 2 that can contain the whole 
+# vector table.  Example: table has beween 65-128 words: alignm to 128*4 = 512B.
+# Regardless of VT alignment requirement, the minimum IMAGE_HEADER_SIZE is 256.
 
 if(${MCU_VARIANT} STREQUAL "XMC4700")
   set(PARTITION_SIZE              0x40000) # 256kB
@@ -16,6 +32,7 @@ if(${MCU_VARIANT} STREQUAL "XMC4700")
   set(PARTITION_UPDATE_ADDRESS        0x0C080000) # sector 10
   set(PARTITION_BACKUP_ADDRESS        0x0C0C0000) # sector 11
   set(TEST_RESULT_ADDRESS             0x0C01C000) # sector 7 (last 16kB sector)
+  set(IMAGE_HEADER_SIZE 1024)
 elseif(${MCU_VARIANT} STREQUAL "stm32l476")
   set(PARTITION_SIZE              0x40000) # 256kB
   set(SECTOR_SIZE                 0x800) # 2kB (sector = erase block)
@@ -26,9 +43,5 @@ elseif(${MCU_VARIANT} STREQUAL "stm32l476")
   set(PARTITION_UPDATE_ADDRESS        0x08080000) # bank 2
   set(PARTITION_BACKUP_ADDRESS        0x080C0000) # bank 2
   set(TEST_RESULT_ADDRESS             0x080FF800) # last page of flash
+  set(IMAGE_HEADER_SIZE 512)
 endif()
-
-# Arm MCUs often have alignment requirements for the vector table.
-# Refer to for Vector Table Offset Register (VTOR) section in MCU reference.
-# IMAGE_HEADER_SIZE must be a multiple of the VT alignment requirement.  
-set(IMAGE_HEADER_SIZE 1024)
--- example/firmware/mgtBoot_adapter.c
+++ example/firmware/mgtBoot_adapter.c
@@ -1,12 +1,21 @@
-#include <flash.h>
+#include <flash_port.h>
+#include <clock_port.h>
 #include <fmt_flash.h>
+#include <fmt_sysInit.h>
 
 int flash_program(uint32_t address, const uint8_t *data, uint32_t len)
 {
   return fmt_flash_write(address, data, len);
 }
 
+// TODO: remove (flash_program handles this)
 int flash_erase(uint32_t address, uint32_t len)
 {
   return fmt_flash_erase(address, len);
+}
+
+int initClock(void)
+{
+  fmt_initSys();
+  return 0;
 }
