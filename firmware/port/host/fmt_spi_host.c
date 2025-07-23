b70d95a21cae94a09891544d2233b0c1afe6a63e
mthielvoldt <mthielvoldt@gmail.com>
2025-07-23 12:58:10 -0400

    fix unit tests

--- firmware/port/host/fmt_spi_host.c
+++ firmware/port/host/fmt_spi_host.c
@@ -1,7 +1,14 @@
 // This is a MFG-specific header file name. 
+#include <fmt_spi.h>
+#include <stdbool.h>
 #include <stdint.h>
 
 uint32_t port_getSpiEventIRQn(uint8_t spiDriverId)
 {
   return spiDriverId + 1;
+}
+
+bool port_initSpiModule(spiCfg_t *cfg)
+{
+  return true;
 }
