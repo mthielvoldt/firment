e06028defb2327e244002516eb8e927d333c6ffc
mthielvoldt <mthielvoldt@gmail.com>
2025-08-12 13:17:18 -0400

    bug: Fix MAX_MESSAGE_SIZE_BYTES

--- firmware/fmt_sizes.h
+++ firmware/fmt_sizes.h
@@ -8,11 +8,12 @@
 #define PAYLOAD_POSITION (LENGTH_POSITION + LENGTH_SIZE_BYTES)
 #define CRC_SIZE_BYTES 2U
 #define MAX_PACKET_SIZE_BYTES 64U // Must be less than 259; len sent in 1 byte.
-#define MAX_MESSAGE_SIZE_BYTES (MAX_PACKET_SIZE_BYTES - LENGTH_SIZE_BYTES)
+#define MAX_MESSAGE_SIZE_BYTES \
+  (MAX_PACKET_SIZE_BYTES - LENGTH_SIZE_BYTES - CRC_SIZE_BYTES)
 
-/* in the next line, 14 breaks down as follows:
-4: count, 4 value, 3 for the 3 log types, 2 for top types, 1 prefix.*/
-#define MAX_LOG_TEXT_SIZE (MAX_MESSAGE_SIZE_BYTES - 16)
+/* in the next line, 13 breaks down as follows:
+4B count 4B value, 1B each for types: (count, value, text, sub, top) */
+#define MAX_LOG_TEXT_SIZE (MAX_MESSAGE_SIZE_BYTES - 13)
 #define SEND_QUEUE_LENGTH 10U
 #define RX_QUEUE_LENGTH 9U
 #define MAX_SENDER_PRIORITY 16U
