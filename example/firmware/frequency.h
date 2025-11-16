15b0c193dee07b729fa92f714cadfb517e8dec05
mthielvoldt <mthielvoldt@gmail.com>
2025-11-16 09:59:05 -0500

    Tweak gp_streamPeriodic call freq to support 2 variables

--- example/firmware/frequency.h
+++ example/firmware/frequency.h
@@ -5,7 +5,10 @@
  * - "TICKS" represent a duration as measured in ticks of a configured timer.
  */
 
-#define PERIODIC_A_PERIOD_US 1000U
+#define PERIODIC_A_FREQ 1000U
+#define PERIODIC_A_PERIOD_US (1000000U / PERIODIC_A_FREQ)
 #define WAVE_UPDATE_FREQ 1000.0F
 #define GP_SNAP_FREQ 1000U
-#define GP_STREAM_FREQ 100U
+#define GP_STREAM_FREQ 200U
+
+#define GP_STREAM_DIVIDER (PERIODIC_A_FREQ / GP_STREAM_FREQ )
